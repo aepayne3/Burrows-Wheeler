34afbf91a697eac0ce17fb52e362d2a23fe73d97
aepayne3 <aepayne3@crimson.ua.edu>
2018-08-07 19:43:13 -0400

    Add files via upload

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -1,56 +1,56 @@
-import edu.princeton.cs.algs4.Merge.java;
+import edu.princeton.cs.algs4.Merge;
 
 public class CircularSuffixArray {
-	private final int n;
-	private Suffix[] suffixes;
-	private int[] index;
+ private final int n;
+ private Suffix[] suffixes;
+ private int[] index;
 
-	public CircularSuffixArray(String s) {
-		// Initialize size n
-		this.n = s.length();
+ public CircularSuffixArray(String s) {
+  // Initialize size n
+  this.n = s.length();
 
-		// Initialize suffixes array
-		this.suffixes = new Suffix[n];
-		for (int i = 0; i < n; i++) {
-			suffixes[i] = new Suffix(s, i);
-		}
+  // Initialize suffixes array
+  this.suffixes = new Suffix[n];
+  for (int i = 0; i < n; i++) {
+   suffixes[i] = new Suffix(s, i);
+  }
 
-		// Sort suffixes
-		Merge.sort(suffixes);
+  // Sort suffixes
+  Merge.sort(suffixes);
 
-		// Initialize index array
-		this.index[i] = new int[n];
-		for (int i = 0; i < n; i++) {
-			index[suffixes[i].getIndex()] = i;
-		}
-	}
+  // Initialize index array
+  this.index = new int[n];
+  for (int i = 0; i < n; i++) {
+   index[suffixes[i].getIndex()] = i;
+  }
+ }
 
-	public int length() {return n;}
+ public int length() {return n;}
 
-	public int index(int i) {return index[i];}
+ public int index(int i) {return index[i];}
 
-	private class Suffix implements Comparable<Suffix> {
-		private static final String s;
-		private final int index;
+ private static class Suffix implements Comparable<Suffix> {
+  private static String s;
+  private final int index;
 
-		public Suffix(String s, int index) {
-			this.s = s;
-			this.index = index;
-		}
+  public Suffix(String s, int index) {
+   this.s = s;
+   this.index = index;
+  }
 
-		public int getIndex() {return index;}
+  public int getIndex() {return index;}
 
-		// Comparator for Mergesort
-		public int compareTo(Suffix that) {
-			if (this == that) return 0;
-			int n = this.s.length();
-			for (int i = 0; i < n; i++) {
-				char cthis = this.s.charAt((this.index + i) % n);
-				char cthat = that.s.charAt((that.index + i) % n);
-				if (cthis < cthat) {return -1;}
-				if (cthis > cthat) {return 1;}
-			}
-			return 0;
-		}
-	}
+  // Comparator for Mergesort
+  public int compareTo(Suffix that) {
+   if (this == that) return 0;
+   int n = this.s.length();
+   for (int i = 0; i < n; i++) {
+    char cthis = this.s.charAt((this.index + i) % n);
+    char cthat = that.s.charAt((that.index + i) % n);
+    if (cthis < cthat) {return -1;}
+    if (cthis > cthat) {return 1;}
+   }
+   return 0;
+  }
+ }
 }
--- MoveToFront.java
+++ MoveToFront.java
@@ -1,61 +1,61 @@
-import edu.princeton.cs.algs4.BinaryStdIn
-import edu.princeton.cs.algs4.BinaryStdOut
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class MoveToFront {
-	public static void encode() {
-		// Initialize ascii ordered character array
-		char[] letters = new char[256];
-		for (int i = 0; i < 256; i++) {
-			letters[i] = (char)i;
-		}
+ public static void encode() {
+  // Initialize ascii ordered character array
+  char[] letters = new char[256];
+  for (int i = 0; i < 256; i++) {
+   letters[i] = (char)i;
+  }
 
-		// Process input
-		while (!BinaryStdIn.isEmpty()) {
-			// Read character
-			char c = BinaryStdIn.readChar(8);
+  // Process input
+  while (!BinaryStdIn.isEmpty()) {
+   // Read character
+   char c = BinaryStdIn.readChar(8);
 
-			// Find index of current character
-			int index = -1;
-			for (int i = 0; letters[i] != c; i++) {
-				index = i;
-			}
-			index++;
+   // Find index of current character
+   int index = -1;
+   for (int i = 0; letters[i] != c; i++) {
+    index = i;
+   }
+   index++;
 
-			// Output encoded character
-			BinaryStdOut.write(index);
-			BinaryStdOut.flush();
+   // Output encoded character
+   BinaryStdOut.write(index);
+   BinaryStdOut.flush();
 
-			// Swap recently encoded character to front of array
-			for (int i = index; i - 1 >= 0; i--) {
-				char temp = letters[i - 1];
-				letters[i - 1] = letters[i];
-				letters[i] = temp;
-			}
-		}
-	}
+   // Swap recently encoded character to front of array
+   for (int i = index; i - 1 >= 0; i--) {
+    char temp = letters[i - 1];
+    letters[i - 1] = letters[i];
+    letters[i] = temp;
+   }
+  }
+ }
 
-	public static void decode() {
-		// Initialize ascii ordered character array
-		char[] letters = new char[256];
-		for (int i = 0; i < 256; i++) {
-			letters[i] = (char)i;
-		}
-		
-		// Process input
-		while (!BinaryStdIn.isEmpty()) {
-			// Read integer
-			int index = BinaryStdIn.readInt(8);
-			
-			// Output decoded character
-			BinaryStdOut.write(letters[index]);
-			BinaryStdOut.flush();
-			
-			// Swap recently decoded character to front of array
-			for (int i = index; i - 1 >= 0; i--) {
-				char temp = letters[i - 1];
-				letters[i - 1] = letters[i];
-				letters[i] = temp;
-			}
-		}
-	}
+ public static void decode() {
+  // Initialize ascii ordered character array
+  char[] letters = new char[256];
+  for (int i = 0; i < 256; i++) {
+   letters[i] = (char)i;
+  }
+  
+  // Process input
+  while (!BinaryStdIn.isEmpty()) {
+   // Read integer
+   int index = BinaryStdIn.readInt(8);
+   
+   // Output decoded character
+   BinaryStdOut.write(letters[index]);
+   BinaryStdOut.flush();
+   
+   // Swap recently decoded character to front of array
+   for (int i = index; i - 1 >= 0; i--) {
+    char temp = letters[i - 1];
+    letters[i - 1] = letters[i];
+    letters[i] = temp;
+   }
+  }
+ }
 }
