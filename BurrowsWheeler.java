657ddda8ddd0d993ca1581b84f68aff092306ed5
ElaineSchutte <36045471+ElaineSchutte@users.noreply.github.com>
2018-08-08 17:34:15 -0400

    Add files via upload

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -26,5 +26,32 @@ public class BurrowsWheeler {
  }
  
  public static void inverseTransform(){
-   
- }
+        int first = BinaryStdIn.readInt();
+        String t = BinaryStdIn.readString();
+        int n = t.length();
+        int[] count = new int[R + 1], next = new int[n];
+        for (int i = 0; i < n; i++)
+            count[t.charAt(i) + 1]++;
+        for (int i = 1; i < R + 1; i++)
+            count[i] += count[i - 1];
+        for (int i = 0; i < n; i++)
+            next[count[t.charAt(i)]++] = i;
+        for (int i = next[first], c = 0; c < n; i = next[i], c++)
+            BinaryStdOut.write(t.charAt(i));
+        BinaryStdOut.close();
+    }
+ 
+ 
+  public static void main(String[] args) {
+        if (args.length != 1)
+            throw new IllegalArgumentException("Expected + or -\n");
+        else if (args[0].equals("+"))
+            decode();
+        else if (args[0].equals("-"))
+            encode();
+        else {
+            String msg = "Unknown argument: " + args[0] + "\n";
+            throw new IllegalArgumentException(msg);
+        }
+    }
+}
