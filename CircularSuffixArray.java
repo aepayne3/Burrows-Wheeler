c86795c433bb32242560138f095f3c1231ce9751
aepayne3 <aepayne3@crimson.ua.edu>
2018-08-08 21:26:00 -0400

    javadoccs

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -23,13 +23,19 @@ public class CircularSuffixArray {
   this.index = new int[n];
   for (int i = 0; i < n; i++) {
    index[i] = suffixes[i].getIndex();
-    //  System.out.println(suffixes[i].getIndex());
-      //System.out.println();
   }
  }
-
+/*
+ * @return int length of s
+ * return the legth of the suffix
+ */
  public int length(){return n;}
 
+ /*
+  * @param i index to get item from
+  * @return int item at index i
+  * returns the item at index 1
+  */
  public int index(int i) {
      if (i < 0) {throw new IllegalArgumentException();}
      if (i > this.n-1) {throw new IllegalArgumentException();}
@@ -40,17 +46,30 @@ public class CircularSuffixArray {
   private String s;
   private final int index;
 
+  /*
+   * @param s the suffix to be initialized
+   * @param index the index of the suffix
+   */
   private Suffix(String s, int index) {
       if ( s == null) {throw new IllegalArgumentException();}
       if (index < 0) {throw new IllegalArgumentException();}
-     // if (index > 0) {throw new IllegalArgumentException();}
+      if (index > n) {throw new IllegalArgumentException();}
       this.s = s;
       this.index = index;
   }
 
+  /*
+   * @return int index
+   * return the index of the suffix
+   */
   public int getIndex() {return index;}
 
   // Comparator for Mergesort
+  /*
+   * @param that suffix to compare to the suffix of the object
+   * @return int -1 if that is greater than the object, 1 if that is less than the object, and 0 if they are the same
+   * Interface to compare two suffixes
+   */
   public int compareTo(Suffix that) {
       if (that == null) {throw new IllegalArgumentException();}
    if (this == that) return 0;
@@ -69,7 +88,7 @@ public class CircularSuffixArray {
     String test=args[0];
     CircularSuffixArray circSA=new CircularSuffixArray(test);
     int n=test.length();
-    for (int i=0; i<n; i++) {
+    for (int i=0; i<test.length(); i++) {
       System.out.println(circSA.index(i));}
   }
 }
